45dc40364b7e57e16c80a1d0b600ee73064a0245
JoeydeJonge <jd.dejonge@student.avans.nl>
2021-04-19 12:53:33 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+
+const hostname = '127.0.0.1'
+const port = process.env.PORT || 3000
+
+let result = {
+    'response': 'Hello World',
+    'status': 'Alles OK!'
+}
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(result))
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
