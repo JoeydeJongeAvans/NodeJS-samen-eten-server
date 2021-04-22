b6a5ea0807ce919bb5bd3771c3ad511f2181edef
JoeydeJonge <jd.dejonge@student.avans.nl>
2021-04-22 10:33:56 +0200

    Fixed index and add 200 code to api info

--- server.js
+++ server.js
@@ -13,103 +13,105 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/info", (req, res) => {
-  res.send(importData);
+    res.status(200).send(importData);
 });
 
 app.post("/api/studenthome", (req, res) => {
-  let studenhome = {
-    homeid: studenthomes.length + 1,
-    name: req.body.name,
-    streetname: req.body.streetname,
-    number: req.body.number,
-    zipcode: req.body.zipcode,
-    city: req.body.city,
-    phonenumber: req.body.phonenumber,
-  };
+    let studenhome = {
+        homeid: studenthomes.length + 1,
+        name: req.body.name,
+        streetname: req.body.streetname,
+        number: req.body.number,
+        zipcode: req.body.zipcode,
+        city: req.body.city,
+        phonenumber: req.body.phonenumber,
+    };
 
-  if (studenhome) {
-    studenthomes.push(studenhome);
-    res.status(201).send(studenhome);
-  }
-  res.status(400).send("Didnt work lmao");
+    if (studenhome) {
+        studenthomes.push(studenhome);
+        res.status(201).send(studenhome);
+    }
+    res.status(400).send("Didnt work lmao");
 });
 
 //Verwijderen op homeID
 app.delete("/api/studenthome", (req, res) => {
-  console.log(req.query);
-  const { homeid } = req.query;
-  var post;
-  if (homeId) {
-    post = studenthomes.find((post) => post.homeid === homeid);
-    if (post) res.status(200).send(post);
-    else res.status(400).send(`Can't delete studenthome`);
-  }
-  res.status(200).send(importStudenthuis.studenthomes);
+    console.log(req.query);
+    const { homeid } = req.query;
+    var post;
+    if (homeId) {
+        post = studenthomes.find((post) => post.homeid === homeid);
+        if (post)
+            res.status(202).send(post);
+        else
+            res.status(400).send(`Can't delete studenthome`);
+    }
+    res.status(200).send(importStudenthuis.studenthomes);
 });
 
 //Zoeken op homeID
 app.get("/api/studenthome", (req, res) => {
-  console.log(req.query);
-  const { homeId } = req.query;
-  var post;
-  if (homeId) {
-    post = studenthomes.find((post) => post.homeid === homeid);
-    if (post) res.status(200).send(post);
-    else res.status(400).send(`Not Found`);
-  }
+    console.log(req.query);
+    const { homeId } = req.query;
+    var post;
+    if (homeId) {
+        post = studenthomes.find((post) => post.homeid === homeid);
+        if (post) res.status(200).send(post);
+        else res.status(400).send(`Not Found`);
+    }
 });
 
 //Update studenthome
 app.put("/api/studenthome/:homeid", (req, res) => {
-  let homeid = req.params.homeid;
+    let homeid = req.params.homeid;
 
-  let studenthome = studenthomes.filter((studenthome) => {
-    return studenthome.homeid == homeid;
-  })[0];
+    let studenthome = studenthomes.filter((studenthome) => {
+        return studenthome.homeid == homeid;
+    })[0];
 
-  const index = studenthomes.indexOf(studenthome);
-  let keys = Object.keys(req.body);
+    const index = studenthomes.indexOf(studenthome);
+    let keys = Object.keys(req.body);
 
-  keys.forEach((key) => {
-    studenthome[key] = req.body[key];
-  });
-  studenthomes[index] = studenthome;
+    keys.forEach((key) => {
+        studenthome[key] = req.body[key];
+    });
+    studenthomes[index] = studenthome;
 
-  res.json(studenthomes[index]);
-  res.status(201).send(studenthome);
+    res.json(studenthomes[index]);
+    res.status(201).send(studenthome);
 });
 
 //Zoeken op Naam - plaats
 app.get("/api/studenthome", (req, res) => {
-  console.log(req.query);
-  const { city } = req.query;
-  const { name } = req.query;
-  console.log(city);
-  console.log(name);
-  var post;
-  var post2;
-  if (name) {
-    post = studenthomes.filter((post) => post.name.startsWith(name));
-  }
-  console.log(post);
-  if (city) {
-    if (post != null) {
-      post2 = post.filter((post2) => post2.city == city);
-    } else {
-      post2 = studenthomes.filter((post2) => post2.city == city);
+    console.log(req.query);
+    const { city } = req.query;
+    const { name } = req.query;
+    console.log(city);
+    console.log(name);
+    var post;
+    var post2;
+    if (name) {
+        post = studenthomes.filter((post) => post.name.startsWith(name));
     }
-  }
-  if (post2 != null) {
-    res.status(200).send(post2);
-  } else {
-    if (post != null) {
-      res.status(200).send(post);
-    } else {
-      res.status(404).send("Not Found");
+    console.log(post);
+    if (city) {
+        if (post != null) {
+            post2 = post.filter((post2) => post2.city == city);
+        } else {
+            post2 = studenthomes.filter((post2) => post2.city == city);
+        }
     }
-  }
+    if (post2 != null) {
+        res.status(200).send(post2);
+    } else {
+        if (post != null) {
+            res.status(200).send(post);
+        } else {
+            res.status(404).send("Not Found");
+        }
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port http://Localhost:${port}`);
+    console.log(`Listening on port http://Localhost:${port}`);
 });
